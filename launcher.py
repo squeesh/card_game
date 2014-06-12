377abdbc2a156dd7cdf91db94c6172a7119e0a76
squeesh <squeesh@gmail.com>
2014-06-11 21:42:05 -0400

    changes

--- launcher.py
+++ launcher.py
@@ -1,11 +1,11 @@
 class Launcher(object):
     @staticmethod
     def start_game():
-        from gin.multiplayer.controller import LocalRemoteGinController
+        from gin.console.controller import AllLocalGinController as Controller
 
-        print('Welcome to shitty Gin', ' '.join(LocalRemoteGinController.SUITS))
+        print('Welcome to shitty Gin', ' '.join(Controller.SUITS))
         print('Waiting for player to connect...')
 
-        ctrl = LocalRemoteGinController.get()
+        ctrl = Controller.get()
         ctrl.create_game()
         ctrl.main_loop()
